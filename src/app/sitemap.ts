1a3200f4a894ce3f24cf030b9acbc9f141562c85
benji <benjichukwudile@gmail.com>
2025-11-25 00:56:24 +0000

    update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,19 +4,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://eastcourt.vercel.app/",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
       url: "https://eastcourt.vercel.app/about_us",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: "https://eastcourt.vercel.app/properties",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
